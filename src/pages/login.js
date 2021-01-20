d0a975f261d87b84c29dcfa305641e0702563127
liubo <liubo@coship.com>
2021-01-20 18:08:51 +0800

    注册功能

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,18 +1,38 @@
 import React, { useState } from 'react';
 import './login.less';
-import { Button, List, InputItem } from 'antd-mobile';
+import { Button, List, InputItem, WhiteSpace } from 'antd-mobile';
 
 const LoginPage = () => {
+  const [info, setInfo] = useState({
+    type: '0',
+    title: '登录信息',
+    btn: '登录',
+  });
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
 
   const loginSubmit = () => {
     console.log({ name, password });
   };
+
+  const register = () => {
+    setInfo(() => ({
+      type: '1',
+      title: '注册信息',
+      btn: '注册',
+    }));
+  };
+  const backLogin = () => {
+    setInfo(() => ({
+      type: '0',
+      title: '登录信息',
+      btn: '登录',
+    }));
+  };
   return (
     <div style={{ padding: '15px 0' }}>
       <img className="loginbanner" src={require('@/img/loginpic.jpeg')} />
-      <List renderHeader={() => <h5>登录信息</h5>}>
+      <List renderHeader={() => <h5>{info.title}</h5>}>
         <InputItem
           value={name}
           clear
@@ -31,12 +51,49 @@ const LoginPage = () => {
         </InputItem>
       </List>
       <Button
-        style={{ marginTop: '25px' }}
+        style={{ marginTop: '15px' }}
         type="primary"
         onClick={loginSubmit}
       >
-        登录
+        {info.btn}
       </Button>
+      <WhiteSpace></WhiteSpace>
+      {info.type == '0' && (
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button
+            type="ghost"
+            size="small"
+            className="am-button-borderfix"
+            inline
+          >
+            忘记密码
+          </Button>
+          <Button
+            type="ghost"
+            size="small"
+            className="am-button-borderfix"
+            icon="check-circle-o"
+            inline
+            onClick={register}
+          >
+            前往注册
+          </Button>
+        </div>
+      )}
+      {info.type == '1' && (
+        <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+          <Button
+            type="ghost"
+            size="small"
+            className="am-button-borderfix"
+            icon="check-circle-o"
+            inline
+            onClick={backLogin}
+          >
+            返回登录
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
